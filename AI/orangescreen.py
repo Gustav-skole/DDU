2e7789470b46947c7f6fe513bd6b86ba0da72959
Gustav Mark-Hansen <45123074+GustavMH@users.noreply.github.com>
2019-10-05 19:33:01 +0200

    functionel koordinat generator *

--- AI/orangescreen.py
+++ AI/orangescreen.py
@@ -25,10 +25,11 @@ def remove_isolated_pixels(image):
     return new_image
 
 cap = cv2.VideoCapture(0)
-#frame = cv2.imread("color.jpg")
+frame = cv2.imread("nudel.png")
 while(True):
+    black_image = np.zeros(shape=frame.shape, dtype=np.uint8)
 
-    ret, frame = cap.read()
+    #ret, frame = cap.read()
     flip = cv2.flip(frame,1)
     mask_rg = remove_isolated_pixels(cv2.inRange(flip, np.array([0,83,212]) , np.array([100,242,255])))
 
@@ -36,32 +37,34 @@ while(True):
     x, y, w, h = cv2.boundingRect(absolute)
     if h >= 15:
         rect = (x, y, w, h)
-        cv2.rectangle(flip, (x, y), (x+w, y+h), (0, 255, 0), 1)
+        #cv2.rectangle(black_image, (x, y), (x+w, y+h), (0, 255, 0), 1)
 
     boundsArray = []
-    boxImage = absolute[x:(x+w),y:(y+h)]
+
+    boxImage = absolute[y:(y+h),x:(x+w)]
+    ssw = 1/8*w
 
     for i in range(8):
-        Sw = i/8*w
-        sliceImage = boxImage[int(Sw):int(Sw+1/8*w),0:h]
-        ix, iy, iw, ih = cv2.boundingRect(sliceImage)#boxImage[int(Sw):int(Sw+1/8*w),0:h])
-        point = (x+ix-int(iw/2),y+int(Sw+1/8*w)+iy-int(ih/2))
-        boundsArray.append(point)
-        if i == 6:
-            print(sliceImage.shape)
-            print(x, y, w, h," i: ",ix,iy,iw,ih)
+        sliceImage = boxImage[0:h,int(ssw*i):int(ssw*(i+1))]
+        print(sliceImage.shape,x,y,w,h)
+        ix, iy, iw, ih = cv2.boundingRect(sliceImage)
+        #cv2.rectangle(black_image, (ix+x+int(ssw*i), iy+y), (x+ix+iw+int(ssw*i), y+iy+ih), (0, 255, 0), 1)
+        pointY = int(iy+y+ih/2)
+        pointX = int(x+ix+i*ssw+iw/2)
+
+        boundsArray.append((int(pointX),int(pointY)))
 
     for i in range(7):
         #print(boundsArray[i],boundsArray[1+i])
-        cv2.line(flip,boundsArray[i],boundsArray[1+i],(0,255,0),1)
+        cv2.line(black_image,boundsArray[i],boundsArray[1+i],(0,255,0),1)
 
     width, height, channels = flip.shape
     # (((width - height)/2)+height)
     #
     cropped = flip[80:480,0:400]
     scaled = cv2.resize(cropped,(72,72))
-
-    cv2.imshow("new", cropped)
+    cv2.imshow("new", cv2.addWeighted(black_image,0.5,cv2.cvtColor(absolute, cv2.COLOR_GRAY2RGB),0.5,0))
+    #cv2.imshow("new", cv2.add(black_image, cv2.cvtColor(absolute, cv2.COLOR_GRAY2RGB)))
     #cv2.imshow('Frame', cv2.add(cv2.cvtColor(mask_rg,cv2.COLOR_GRAY2RGB), flip))
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
